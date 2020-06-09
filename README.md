517d78e5e369ae6b430c1277cfa3cdb22ae33b05
CodyAdam <cody.adam@laposte.net>
2020-06-09 13:42:35 +0200

    update readme + make seperated files

--- README.md
+++ README.md
@@ -2,13 +2,15 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 ![alt text](https://github.com/[username]/[reponame]/blob/[branch]/image.jpg?raw=true)
 
-### Try the Demo Here : [codyadam.github.io/React-Resizable-Panel](https://codyadam.github.io/React-Resizable-Panel/)
+## Try the Demo Here : [codyadam.github.io/React-Resizable-Panel](https://codyadam.github.io/React-Resizable-Panel/)
 
 # What is this ?
 
 This is a single React component that allow you to make custom resize bar for your panels.
 
 # Demo
+
+`import ResizeBar from "./ResizeBar.js";`
 
 ![Screen of the JSX code](https://github.com/CodyAdam/React-Resizable-Panel/blob/master/ScreenCode.png?raw=true)
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+import ResizeBar from "./ResizeBar.js";
+
 class App extends React.Component {
     render() {
         return (
             <div id="App">
-                {/* <ResizeBar direction="vertical" position={66}>
+                <ResizeBar direction="vertical" position={66}>
                     <ResizeBar direction="horizontal">
                         <div id="div1">Div 1</div>
                         <div id="div2">
@@ -20,108 +22,7 @@ class App extends React.Component {
                         </div>
                     </ResizeBar>
                     <div id="div3">Div 3</div>
-                </ResizeBar> */}
-
-                <ResizeBar direction="vertical">
-                    <div id="div1">Div 1</div>
-                    <div id="div2">Div 2</div>
                 </ResizeBar>
-            </div>
-        );
-    }
-}
-
-/**
- * Add a resize bar between two panel (can not handle more or less then 2 childrens)
- * @param direction either "vertical" or "horizontal" (horizontal by default)
- * @param position area where the bar should be placed by default in percent (50% by default)
- * @param className the className of the bar to change it's style ("resizeBar" by default)
- * @param minPos the minimum % position of the bar (10% by default)
- * @param maxPos the maximum % position of the bar (90% by default)
- * @return the two children with the resize bar in between
- */
-class ResizeBar extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.onMouseDown = this.onMouseDown.bind(this);
-        this.onMouseMove = this.onMouseMove.bind(this);
-        this.onMouseUp = this.onMouseUp.bind(this);
-        this.setContainerSize = this.setContainerSize.bind(this);
-
-        this.container = React.createRef();
-        this.state = {
-            containerSize: null,
-            mouseStartPos: null,
-            position: props.position ? props.position : 50,
-            minPos: props.minPos ? props.minPos : 10,
-            maxPos: props.maxPos ? props.maxPos : 90,
-            dragging: false,
-        };
-    }
-
-    onMouseDown(e) {
-        this.setContainerSize();
-        this.setState({ dragging: true, mouseStartPos: this.props.direction === "vertical" ? e.clientX : e.clientY });
-    }
-    onMouseMove(e) {
-        if (this.state.dragging) {
-            const mousePos = this.props.direction === "vertical" ? e.clientX : e.clientY;
-            const containerSize = this.state.containerSize;
-            const defaultPos = this.props.position ? this.props.position : 50;
-
-            const newPosition = defaultPos - ((containerSize * defaultPos * 0.01 - mousePos) * 100) / containerSize;
-
-            this.setState({ position: Math.min(Math.max(newPosition, this.state.minPos), this.state.maxPos) });
-        }
-    }
-    onMouseUp(e) {
-        this.setState({ dragging: false });
-    }
-
-    setContainerSize() {
-        this.setState({
-            containerSize:
-                this.props.direction === "vertical"
-                    ? this.container.current.offsetWidth
-                    : this.container.current.offsetHeight,
-        });
-    }
-
-    componentDidMount() {
-        this.setContainerSize();
-        window.addEventListener("resize", this.setContainerSize);
-    }
-
-    render() {
-        const containerStyle = {
-            width: "100%",
-            height: "100%",
-            display: "grid",
-            flexGrow: "1",
-            gridTemplateColumns: this.props.direction === "vertical" ? `${this.state.position}% 5px auto` : "auto",
-            gridTemplateRows: this.props.direction === "vertical" ? "auto" : `${this.state.position}% 5px auto`,
-        };
-        const resizeBarStyle = {
-            height: this.props.direction === "vertical" ? "100%" : "5px",
-            width: this.props.direction === "vertical" ? "5px" : "100%",
-            cursor: this.props.direction === "vertical" ? "ew-resize" : "ns-resize",
-        };
-        return (
-            <div
-                style={containerStyle}
-                ref={this.container}
-                onMouseMove={this.onMouseMove}
-                onMouseUp={this.onMouseUp}
-                onMouseLeave={this.onMouseUp}
-            >
-                {this.props.children[0]}
-                <div
-                    style={resizeBarStyle}
-                    className={this.props.className ? this.props.className : "resizeBar"}
-                    onMouseDown={this.onMouseDown}
-                />
-                {this.props.children[1]}
             </div>
         );
     }
